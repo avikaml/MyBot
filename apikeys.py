0b6cd80910ff082bbe4ee8ad108a67c0d67686e8
avikaml <avikaml@post.bgu.ac.il>
2023-08-01 22:12:23 +0300

    Start of the bot

---
+++ apikeys.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+BOT_TOKEN = 'MTEzNTk5NzIzNjE1Nzg5NDc5Ng.GvRD1u.NHJGN3am2lFSohfFsmFwV2N1LqyVsH9gFcLF7A'
+
+# Bot ID : 1135997236157894796

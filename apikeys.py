4e81b3ffbf03c3dfd318eeff8221e3ff677b04c8
avikaml <avikaml@post.bgu.ac.il>
2023-08-02 00:55:16 +0300

    Minor changes and additions
    Edited readme files
    Added minor things in main and apikeys

--- apikeys.py
+++ apikeys.py
@@ -5,3 +5,5 @@ from discord.ext import commands
 BOT_TOKEN = 'MTEzNTk5NzIzNjE1Nzg5NDc5Ng.GvRD1u.NHJGN3am2lFSohfFsmFwV2N1LqyVsH9gFcLF7A'
 
 # Bot ID : 1135997236157894796
+
+# General Chat ID: 1136000176016871596

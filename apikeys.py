471292c08251f2009042b828e0271a2ee7e846fe
avikaml <avikaml@post.bgu.ac.il>
2023-08-04 17:31:52 +0300

    minor changes

--- apikeys.py
+++ apikeys.py
@@ -1,6 +1,3 @@
-import discord
-from discord.ext import commands
-
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 BOT_TOKEN = 'MTEzNTk5NzIzNjE1Nzg5NDc5Ng.GvRD1u.NHJGN3am2lFSohfFsmFwV2N1LqyVsH9gFcLF7A'
 

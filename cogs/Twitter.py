4f8970a209ed2a9f0ec4d6bfb856cf9172e523b0
avikaml <avikaml@post.bgu.ac.il>
2023-08-06 16:58:20 +0300

    added !vx for twitter embeds

--- cogs/Twitter.py
+++ cogs/Twitter.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import tweepy
 import settings
+import re
 
 class Twitter(commands.Cog):
 
@@ -19,9 +20,27 @@ class Twitter(commands.Cog):
 
     @commands.Cog.listener() 
     async def on_ready(self):
-        print('Twitter.py is ready')
+        print('Twitter.py is ready but only !vx works')
 
-    @commands.command(alias=['t'])
+    @commands.command()
+    async def vx(self, ctx, tweet_url):
+        # Check if the URL contains "twitter" and replace it with "vxtwitter"
+        vxtwitter_url = tweet_url.replace("twitter", "vxtwitter")
+
+        # Send the modified URL back to the user
+        await ctx.send(vxtwitter_url)
+
+        messages = []
+        async for message in ctx.channel.history(limit=100):
+            messages.append(message)
+
+        for message in messages:
+            if message.author == ctx.message.author and message.embeds:
+                for embed in message.embeds:
+                    if "twitter.com" in embed.url:
+                        await message.delete()
+
+    @commands.command()
     async def tweet(self, ctx, tweet_url):
         try:
             # Extract the tweet ID from the tweet URL
@@ -53,3 +72,10 @@ async def setup(client):
     access_token = settings.twitter_access_token
     access_token_secret = settings.twitter_access_token_secret
     await client.add_cog(Twitter(client, api_key, api_secret_key, access_token, access_token_secret))
+
+def convert_twitter_links_to_vxtwitter(text):
+    twitter_pattern = r"(https?://twitter\.com/(?:\w+)/(status|statuses)/(\d+))"
+    replacement = r"https://vx.twitter.com/\3"
+
+    vxtwitter_text = re.sub(twitter_pattern, replacement, text)
+    return vxtwitter_text
